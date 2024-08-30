1d63ebc3d787060196d7a752e5809b307f3a182e
Chad Barbee <chad_barbee@msn.com>
2024-08-29 20:55:15 -0400

    refactor: remove content from theme code

--- front-page.php
+++ front-page.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
     <section id="hero">
         <div class="background-video-container">
-            <video class="elementor-background-video-hosted elementor-html5-video" autoplay="" muted="" playsinline="" loop=""
-             src="<?php echo(get_site_url()); ?>/wp-content/uploads/2024/08/green-paint-in-water.mov"
+            <video class="" autoplay="" muted="" playsinline="" loop=""
+             src="<?php echo( get_field('hero_media_file',get_the_ID()) ); ?>"
             style="width: 1920px; height: 1080px;"></video>
         </div>
         <div class="container">
@@ -40,52 +40,49 @@
                             &nbsp;
                         </span>
                     </div>
-                    <h3 class="heading-title">Skills.</h3>
+                    <h3 class="heading-title"><?php echo(get_field('intro_headline',get_the_ID())); ?></h3>
 
                 </div>
                 <div class="widget-container">
-                    <h4 class="subheading-title">Over 20+ years of web development experience &amp; insights</h4>
+                    <h4 class="subheading-title"><?php echo(get_field('intro_subhead',get_the_ID())); ?></h4>
                 </div>
-
-                 
 
             </section>
         </div>
 
+        <?php
+         $cards = get_field('homepage_cards',get_the_ID());
+         if($cards) :
+        ?>
         <div class="container">
-           <div class="row cards">
-                <div class="col card">
-                    <div class="card-icon">
-                        <?php echo(file_get_contents( get_site_url() . '/wp-content/themes/chads-resume/assets/images/coding-html.svg')); ?>
+            <div class="row cards">
+                <?php
+                foreach ( $cards as $key => $card ) {
+                    $last_class = ($key === array_key_last($cards)) ? 'last' : '';
+                ?>
+                    <div class="col card <?php echo($last_class); ?>">
+                        <div class="card-icon">
+                            <?php echo( file_get_contents( get_template_directory_uri() . '/assets/images/'. $card['card_icon'] ) ); ?>
+                        </div>
+                        <h5><?php echo($card['card_headline']); ?></h5>
+                        <p><?php echo($card['card_content']); ?></p>
                     </div>
-                    <h5>FrontEnd Design</h5>
-                    <p>Anyone can design a beautiful website. I can build one that functions. Having built sites using a wide stack of frontend technologies, I have worked with a vast array of clients &amp; industries. I can anticipate the unique challenges you may face.</p>
-                </div>
-                <div class="col card">
-                    <div class="card-icon data-icon">
-                        <?php echo(file_get_contents( get_site_url() . '/wp-content/themes/chads-resume/assets/images/database.svg')); ?>
-                    </div>
-                    <h5>Backend Developement</h5>
-                    <p>Data is boring. But you'd better know how to use it. Breathe easy, I come with a keen understanding of relational databases. I can help you take control of your data and backend processes. Heck, I can even connect your existing systems through API requests.</p>
-                </div>
-                <div class="col card last">
-                    <div class="card-icon">
-                        <?php echo(file_get_contents( get_site_url() . '/wp-content/themes/chads-resume/assets/images/seo-monitoring.svg')); ?>
-                    </div>
-                    <h5>Get Found (SEO)</h5>
-                    <p>I know what robots want. I am able to improve your search ranking so you have the greatest opportunity to excite your customers. I can better your site's web accessibility and boost it's impact on your SEO ranking.</p>
-                </div>
-            </div>
+                <?
+                }
+                ?>
+            </div>    
         </div>
+        <?php
+        endif;
+        ?>
 
     </section>
 
     <section id="hp-slider">
         <div class="container">
-            <?php echo( do_shortcode("[chad_carousel id='93']") ); ?>
+            <?php echo( do_shortcode(get_field('carousel_shortcode',get_the_ID())) ); ?>
         </div>
-        <p class="full-portfolio"><a href="<?php echo(the_permalink(10)) ?>">See my full portfolio <i class="fa-solid fa-arrow-right"></i></a></p>
-    
+        <p class="full-portfolio"><a href="<?php echo(get_field('portfolio_page_link',get_the_ID())); ?>"><?php echo(get_field('portfolio_page_cta',get_the_ID())); ?> <?php echo(get_field('portfolio_cta_icon',get_the_ID())) ?></a></p>
     </section>
 
     
